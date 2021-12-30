7b46639e150ec3e0a0baffe291894cb2236e1e59
Jorge Barcos <jorgebarcos88@gmail.com>
2021-12-30 11:46:48 -0300

    Preparar pantalla de escritorio

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,42 @@
+// Referencias HTML
+const lblEscritorio = document.querySelector('h1');
+const btnAtender = document.querySelector('button')
 
 
-console.log('Escritorio HTML');
+const searchParams = new URLSearchParams(window.location.search);
+
+if (!searchParams.has('escritorio')) {
+    window.location = 'index.html';
+    throw new Error('El escritorio es oblogatorio');
+}
+
+const escritorio = searchParams.get('escritorio'); const socket = io();
+lblEscritorio.innerText = escritorio
+
+
+socket.on('connect', () => {
+
+
+    btnAtender.disabled = false;
+
+});
+
+socket.on('disconnect', () => {
+
+
+    btnAtender.disabled = true;
+});
+
+
+socket.on('ultimo-ticket', (last) => {
+
+    // lblNuevoTicket.innerText = 'Ticket ' + last;
+})
+
+btnAtender.addEventListener('click', () => {
+
+    // socket.emit('siguiente-ticket', null, (ticket) => {
+    //     lblNuevoTicket.innerText = ticket
+    // });
+
+});
--- public/js/nuevo-ticket.js
+++ public/js/nuevo-ticket.js
@@ -9,14 +9,14 @@ const socket = io();
 
 
 socket.on('connect', () => {
-    // console.log('Conectado');
+
 
     btnCrear.disabled = false;
 
 });
 
 socket.on('disconnect', () => {
-    // console.log('Desconectado del servidor');
+
 
     btnCrear.disabled = true;
 });
